15631813159c9e18d23650f1c6e41abf6755d432
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-07-10 11:56:50 +0100

    (MODULES-9294) Use shared_examples to reduce duplicated test setup

--- spec/acceptance/purge/negative/purge_all_perms_file_spec.rb
+++ spec/acceptance/purge/negative/purge_all_perms_file_spec.rb
@@ -6,20 +6,17 @@ describe 'Purge' do
       file { "#{target_parent}":
         ensure => directory
       }
-
       file { "#{target}":
         ensure  => file,
         content => '#{file_content}',
         require => File['#{target_parent}']
       }
-
       user { "#{user_id}":
         ensure     => present,
         groups     => 'Users',
         managehome => true,
         password   => "L0v3Pupp3t!"
       }
-
       acl { "#{target}":
         permissions  => [
           { identity => '#{user_id}', rights => ['full'] },
@@ -51,25 +48,25 @@ describe 'Purge' do
       context "on #{agent}" do
         it 'Execute Apply Manifest' do
           execute_manifest_on(agent, acl_manifest(target, file_content), debug: true) do |result|
-            expect(result.stderr).not_to match(%r{Error:})
+            assert_no_match(%r{Error:}, result.stderr, 'Unexpected error was detected!')
           end
         end
 
         it 'Verify that ACL Rights are Correct' do
           on(agent, verify_acl_command) do |result|
-            expect(result.stdout).to match(%r{#{acl_regex_user_id}})
+            assert_match(acl_regex_user_id, result.stdout, 'Expected ACL was not present!')
           end
         end
 
         it 'Attempt to Execute Purge Manifest' do
           execute_manifest_on(agent, purge_acl_manifest(target), debug: true, exepect_failures: true) do |result|
-            expect(result.stderr).to match(%r{#{verify_purge_error}})
+            assert_match(verify_purge_error, result.stderr, 'Expected error was not detected!')
           end
         end
 
         it 'Verify File Data Integrity' do
           on(agent, verify_content_command) do |result|
-            expect(result.stdout).to match(%r{#{file_content_regex(file_content)}})
+            assert_match(file_content_regex(file_content), result.stdout, 'Expected file content is invalid!')
           end
         end
       end
